9ebbe260af5548ec71be3755ac3e69b29e37f9fa
dmshch <freebsdforever@gmail.com>
2019-12-15 22:33:34 +0300

    Added structure of mapping.

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -1,8 +1,8 @@
 package app;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,26 +10,60 @@ import app.protocols.hls.*;
 import app.protocols.mpegdash.*;
 
 @RestController
-@RequestMapping("/checking")
 public class MainController {
 
-        private static final String template = "OTT stream is: %s!";
+    enum Status {OK, ERROR}
 
-        @GetMapping
-        public BasicStreamOtt checking(@RequestParam(value="url", defaultValue="") String url) {
+    ArrayList<BasicStreamOtt> allStreams = new ArrayList<>();
 
-            BasicStreamOtt stream = null;
-            // Необходимо будет проверить ссылку и вызвать соответствующий конструктор для текущего протокола
-            try {
-                if (url.contains(".m3u8")) {
-                    stream = new LivePlaylistM3u("StreamLive", url);
-                }
-                if (url.contains(".mpd")) {
-                    stream = new LivePlaylistMpd("StreamLive", url);
-                }
-            } catch (IOException e) {
-                url = e.toString();
-            }
-            return stream;
+    private static final String template = "OTT stream is: %s!";
+
+    @RequestMapping("addstream")
+    String addstream(@RequestParam String name, @RequestParam String url) {
+        System.out.println(name + " " + url);
+        // вызов функции добавление нового потока
+        if (add(name, url) == Status.ERROR){
+            return "func addstream error:" + "name="+name+";url="+url;
         }
+        return "func addstream ok:" + "name="+name+";url="+url;
+    }
+
+    // по умолчанию возвращает все потоки, при передаче аргумента name - только запрошенный
+    @RequestMapping("getstream")
+    String getstream(@RequestParam String name) {
+
+        return "getstream";
+    }
+
+    @RequestMapping("deletestream")
+    String deletestream(@RequestParam String name) {
+        // вызов функции удаления потока по указанному имени
+        return "deletestream";
+    }
+
+    // Создаёт объект потока, добавляет его в массив и возвращает статус операции
+    // Сделать проверку для совпадения имён в массиве, они должны быть уникальны
+    public Status add(String name, String url) {
+        BasicStreamOtt stream = null;
+        // Необходимо будет проверить ссылку и вызвать соответствующий конструктор для текущего протокола
+        try {
+            if (url.contains(".m3u8")) {
+                stream = new LivePlaylistM3u(name, url);
+            }
+            if (url.contains(".mpd")) {
+                stream = new LivePlaylistMpd(name, url);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            return Status.ERROR;
+        }
+        allStreams.add(stream);
+        return Status.OK;
+    }
+
+    // Удалает из ArrayList объект потока
+    public Status delete(String name){
+        return Status.OK;
+    }
+
 }
